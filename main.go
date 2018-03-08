76aa28380c2c3a28264eaabc1efbaaeb4996a4e4
Aniket <aniket.gadre@sap.com>
2018-03-08 12:09:07 +0800

    move protobuffers

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net"
 	"time"
 
-	pb "github.com/coding-yogi/grpcserver/protobuffers"
+	pb "github.com/coding-yogi/grpcserver/employee"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
